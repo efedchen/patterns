f0e027d79da9479709f406d3a7695beb045a9e1d
jade <eugeniusz.fed@gmail.com>
2017-09-07 22:42:08 +0200

    fixed error

---
+++ PrototypePattern/src/SandwichPrototype.java
@@ -0,0 +1,13 @@
+public abstract class SandwichPrototype implements Cloneable {
+    public Object clone(){
+        Object clone  = null;
+
+        try{
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return clone;
+    }
+}
